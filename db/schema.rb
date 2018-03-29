f46f44a68e9f5677a2bff4ecbd2ed442802b614b
gauravano <sachdeva.gaurav1997@gmail.com>
2018-03-30 02:50:05 +0530

    department added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329211343) do
+ActiveRecord::Schema.define(version: 20180329211917) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "email", default: "", null: false
