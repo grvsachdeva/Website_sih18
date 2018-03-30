9b1f0213da21c0862e6e13aa9c72b834ba1098ee
gauravano <sachdeva.gaurav1997@gmail.com>
2018-03-30 19:57:37 +0530

    mailer view

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,9 @@
 class NotificationMailer < ApplicationMailer
   default from: "sachdevarockz@gmail.com"
 
-  def send_notification(employee,message)
+  def send_notification(employee, sender, message)
     @employee = employee
+    @sender = sender
     @message = message
     mail(to: @employee.email, subject: "Please send your current position update #{ @employee.name }" )
   end
