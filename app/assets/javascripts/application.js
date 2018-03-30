1210bd9d887c55ec8e421d64e21a082e554dc3e2
gauravano <sachdeva.gaurav1997@gmail.com>
2018-03-30 09:22:57 +0530

    materialize added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
